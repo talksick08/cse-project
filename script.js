53d208bdf3c6d0bcdbc43af3bfdd7d640d164575
talksick08 <rahulmahanta156@gmail.com>
2026-06-07 14:48:21 +0530

    Add CSS styles to script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+body{
+text-align:center;
+font-family:Arial;
+background:#f2f2f2;
+}
 
+h1{
+color:#333;
+}
+
+input{
+padding:10px;
+width:250px;
+}
+
+button{
+padding:10px;
+cursor:pointer;
+}
+
+li{
+margin:10px;
+list-style:none;
+}
